07d462c64baf0fd5d28a2319ed377e858e96b1b9
Donia Tung <doniatung1@Donias-MacBook-Pro.local>
2018-10-21 19:50:31 -0400

    basic everything

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+import urllib2, json
+
+my_app = Flask(__name__)
+
+
+@my_app.route('/', methods=['GET', 'POST'])
+def root():
+    data = "http://mappy.dali.dartmouth.edu/members.json"
+    u = urllib2.urlopen(data)
+    d = json.loads(u.read())
+    display = {}
+    counter = 0
+    while(len(d) >counter):
+        person = d[counter]["name"]
+        info = {}
+        info["url"] = d[counter]["url"]
+        info["termsOn"] = d[counter]["terms_on"][0]
+        info["iconURL"] = d[counter]["iconUrl"]
+        info["message"] = d[counter]["message"]
+        info["lat"] = d[counter]["lat_long"][0]
+        info["long"] = d[counter]["lat_long"][1]
+        if (len(d[counter]["project"]) > 0):
+            info["project"] = d[counter]["project"][0]
+        display[person] = info
+        counter +=1
+
+    return render_template('home1.html', group = " : all", display = display)
+
+
+
+if __name__ == '__main__':
+    my_app.debug = True
+    my_app.run()
