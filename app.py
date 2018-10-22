a40055a922cccddbb478253583ec8d49a9f0ef0b
Donia Tung <doniatung1@Donias-MacBook-Pro.local>
2018-10-21 20:24:16 -0400

    ok this works now

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import urllib2, json
 
 my_app = Flask(__name__)
@@ -10,21 +10,29 @@ def root():
     u = urllib2.urlopen(data)
     d = json.loads(u.read())
     display = {}
+    pdict = {}
     counter = 0
-    while(len(d) >counter):
+    while(len(d) > counter):
         person = d[counter]["name"]
         info = {}
         info["url"] = d[counter]["url"]
         info["termsOn"] = d[counter]["terms_on"][0]
-        info["iconURL"] = d[counter]["iconUrl"]
+        info["iconURL"] = "../static/" + d[counter]["iconUrl"]
         info["message"] = d[counter]["message"]
         info["lat"] = d[counter]["lat_long"][0]
         info["long"] = d[counter]["lat_long"][1]
         if (len(d[counter]["project"]) > 0):
             info["project"] = d[counter]["project"][0]
-        display[person] = info
+        else:
+            info["project"] = "n/a"
+        pdict[person] = info
         counter +=1
-
+    if (request.method =="POST"):
+        for person in pdict:
+            if (pdict[person]["termsOn"] == request.form["term"]):
+                display[person] = pdict[person]
+    else:
+        display = pdict
     return render_template('home1.html', group = " : all", display = display)
 
 
